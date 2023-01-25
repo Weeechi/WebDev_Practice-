86490c569401283a8fb8824476f115f0e96bd2ca
Weechi <Weechi@MacBook-Pro.local>
2023-01-25 16:31:57 -0500

    end of day 2

--- Chapter2/practice.js
+++ Chapter2/practice.js
@@ -7,7 +7,16 @@
 const prompt=require("prompt-sync")({sigint:true}); 
 
 
-var x = prompt("write something here please: ")
-console.log('You have printed ' + x.length +' characters ');
-v = 140 - x.length 
-console.log('You have ' + v + " chracters remaining" );
+// var x = prompt("write something here please: ")
+// console.log('You have printed ' + x.length +' characters ');
+// v = 140 - x.length 
+// console.log('You have ' + v + " chracters remaining" );
+
+// ##########################################
+// var tweet = prompt("What is your Tweet?: ")
+// console.log(tweet.slice(0,10));
+// ###########################################
+var name = prompt('what is your name?: ')
+x = name.slice(0,1)
+x.toUpperCase()
+console.log("Hi Nice to meet you " + x.toUpperCase() + name.slice(1,4));
